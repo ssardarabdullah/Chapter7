90e5a123edd0ccc77799e1fab798b76901133d7d
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-05-31 15:40:42 +0500

    Update 20250531-153612.pychapter7

--- 20250531-153612.py
+++ 20250531-153612.py
@@ -1,3 +1,13 @@
-# 7-1. Rental Car
-car = input("What kind of rental car would you like? ")
-print(f"Let me see if I can find you a {car.title()}.")
+Question 1_______
+Write a program that asks the user
+what kind of rental car they would
+like. Print a message about that car
+, such as “Let me see if I can find
+you a Subaru
+
+solution ______
+ Rental Car
+car = input("What kind of rental 
+car would you like? ")
+   print(f"Let me see if I can 
+  find you a {car.title()}.")
