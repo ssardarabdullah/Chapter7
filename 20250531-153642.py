b03f9040a1f7f18c89a685accfd8c71de5877474
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-05-31 15:46:48 +0500

    Update 20250531-153642.pychapter7

--- 20250531-153642.py
+++ 20250531-153642.py
@@ -1,7 +1,17 @@
-# 7-2. Restaurant Seating
-group_size = int(input("How many people are in your dinner group? "))
+Question 2 ___________
+Write a program that asks the user
+how many people 
+are in their dinner group.
+If the answer is more than eight,
+print a message say￾ing they’ll
+have to wait for a table 
+
+solution ___________
+group_size = int(input("How many
+people are in your dinner group? "))
 
 if group_size > 8:
-    print("You'll have to wait for a table.")
+    print("You'll have to wait 
+    for a table.")
 else:
     print("Your table is ready.")
