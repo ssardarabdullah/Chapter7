a6eb2f54bf445c075599ef6579b4f7ba14c1620a
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-05-31 16:15:36 +0500

    Update 20250531-161256.pychapter7

--- 20250531-161256.py
+++ 20250531-161256.py
@@ -1,3 +1,10 @@
+Question 6___________
+Write a loop that never ends, 
+and run it. (To end the loop, press
+ctrl-C or close the window 
+displaying the output.)
+
+solution _______
 while True:
     print("This loop will run forever!")
     import time
