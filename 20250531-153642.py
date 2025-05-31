4ac1f5e50aa736bbb8bd2ce952ffb7f9882781fa
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-05-31 15:41:42 +0500

    Chapter 7

---
+++ 20250531-153642.py
@@ -0,0 +1,7 @@
+# 7-2. Restaurant Seating
+group_size = int(input("How many people are in your dinner group? "))
+
+if group_size > 8:
+    print("You'll have to wait for a table.")
+else:
+    print("Your table is ready.")
