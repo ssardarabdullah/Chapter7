d00a7c6c5190fc91a780f83b2692d9602406cc8b
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-05-31 16:09:50 +0500

    Update 20250531-160542.pychapter7

--- 20250531-160542.py
+++ 20250531-160542.py
@@ -1,6 +1,14 @@
+Question 5______________
+Use a conditional test in the while statement to stop the loop.
+•	 Use an active variable to
+control how long the loop runs.
+•	 Use a break statement to exit
+the loop when the user enters a 
+'quit' value.
+
+solution _______&&&&
 prompt = "Enter a pizza topping (enter 'quit' to stop): "
 topping = ""
-
 while topping != 'quit':
     topping = input(prompt)
     if topping != 'quit':
