4bc67ff346691c42f6097c540fbcf552aa6c7d59
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-05-31 15:57:55 +0500

    Update 20250531-154944.pychapter7

--- 20250531-154944.py
+++ 20250531-154944.py
@@ -1,5 +1,15 @@
+Question 3__________
+Write a loop that prompts the user
+to enter a series of 
+pizza toppings until they enter a 
+'quit' value. As they enter each 
+topping, 
+print a message saying you’ll add
+that topping to their pizza.
+solution ________________
 while True:
-    topping = input("Enter a pizza topping (or type 'quit' to stop): ")
+    topping = input("Enter a pizza 
+    topping (or type 'quit' to stop): ")
 
     if topping.lower() == 'quit':
         print("Finished making your pizza!")
